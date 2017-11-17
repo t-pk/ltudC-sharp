2ff9dae51e2cf1634e6712c5403e3b9ba3b220ae
WIN-GCDD6G4KV5S\Windows 8.1 Ultimate <tantaj2011vn@gmail.com>
2017-11-17 23:21:20 +0700

    update lại

---
+++ Project_Src/LTQLUD1_DACK_Nhom15/LTQLUD1_DACK_Nhom15/frmDangNhap.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace LTQLUD1_DACK_Nhom15
+{
+    partial class FrmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TextBox textBox1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDangNhap));
+            this.lbTittle = new System.Windows.Forms.Label();
+            this.lbUserName = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.cbNhoPassWord = new System.Windows.Forms.CheckBox();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.btnClosed = new System.Windows.Forms.Button();
+            this.lbCopyright = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.AllowDrop = true;
+            textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            textBox1.Location = new System.Drawing.Point(282, 114);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(212, 31);
+            textBox1.TabIndex = 3;
+            // 
+            // lbTittle
+            // 
+            this.lbTittle.AutoSize = true;
+            this.lbTittle.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.lbTittle.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTittle.ForeColor = System.Drawing.Color.Lime;
+            this.lbTittle.Location = new System.Drawing.Point(187, 25);
+            this.lbTittle.Name = "lbTittle";
+            this.lbTittle.Size = new System.Drawing.Size(371, 27);
+            this.lbTittle.TabIndex = 0;
+            this.lbTittle.Text = "PHẦM MỀM QUẢN LÝ THƯ VIỆN";
+            this.lbTittle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbUserName
+            // 
+            this.lbUserName.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.lbUserName.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbUserName.Image = ((System.Drawing.Image)(resources.GetObject("lbUserName.Image")));
+            this.lbUserName.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbUserName.Location = new System.Drawing.Point(98, 105);
+            this.lbUserName.Margin = new System.Windows.Forms.Padding(0);
+            this.lbUserName.Name = "lbUserName";
+            this.lbUserName.Padding = new System.Windows.Forms.Padding(2);
+            this.lbUserName.Size = new System.Drawing.Size(172, 54);
+            this.lbUserName.TabIndex = 1;
+            this.lbUserName.Text = "Tên Đăng Nhập :";
+            this.lbUserName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbUserName.UseCompatibleTextRendering = true;
+            this.lbUserName.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AllowDrop = true;
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textBox2.Location = new System.Drawing.Point(282, 177);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 31);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.BackColor = System.Drawing.Color.Turquoise;
+            this.btnDangKy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDangKy.FlatAppearance.BorderSize = 0;
+            this.btnDangKy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangKy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnDangKy.ForeColor = System.Drawing.Color.Black;
+            this.btnDangKy.Location = new System.Drawing.Point(309, 329);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(104, 41);
+            this.btnDangKy.TabIndex = 5;
+            this.btnDangKy.Text = "Đăng ký";
+            this.btnDangKy.UseVisualStyleBackColor = false;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
+            // 
+            // cbNhoPassWord
+            // 
+            this.cbNhoPassWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.cbNhoPassWord.Location = new System.Drawing.Point(273, 217);
+            this.cbNhoPassWord.Name = "cbNhoPassWord";
+            this.cbNhoPassWord.Size = new System.Drawing.Size(140, 38);
+            this.cbNhoPassWord.TabIndex = 6;
+            this.cbNhoPassWord.Text = "Nhớ Mật Khẩu";
+            this.cbNhoPassWord.UseVisualStyleBackColor = true;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.BackColor = System.Drawing.Color.Orange;
+            this.btnDangNhap.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDangNhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnDangNhap.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDangNhap.Location = new System.Drawing.Point(548, 137);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(105, 38);
+            this.btnDangNhap.TabIndex = 7;
+            this.btnDangNhap.Text = "Đăng Nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = false;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Calibri", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(259, 291);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 35);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Đăng Ký ủy Quyền ADIM";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(322, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Hoặc ";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.lbPassword.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbPassword.Image = ((System.Drawing.Image)(resources.GetObject("lbPassword.Image")));
+            this.lbPassword.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbPassword.Location = new System.Drawing.Point(89, 159);
+            this.lbPassword.Margin = new System.Windows.Forms.Padding(0);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Padding = new System.Windows.Forms.Padding(2);
+            this.lbPassword.Size = new System.Drawing.Size(146, 53);
+            this.lbPassword.TabIndex = 11;
+            this.lbPassword.Text = "Mật Khẩu :";
+            this.lbPassword.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbPassword.UseCompatibleTextRendering = true;
+            // 
+            // btnClosed
+            // 
+            this.btnClosed.BackColor = System.Drawing.SystemColors.Control;
+            this.btnClosed.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnClosed.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClosed.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.btnClosed.FlatAppearance.BorderSize = 0;
+            this.btnClosed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClosed.Image = ((System.Drawing.Image)(resources.GetObject("btnClosed.Image")));
+            this.btnClosed.Location = new System.Drawing.Point(730, 12);
+            this.btnClosed.Margin = new System.Windows.Forms.Padding(3, 3, 10, 3);
+            this.btnClosed.Name = "btnClosed";
+            this.btnClosed.Size = new System.Drawing.Size(30, 30);
+            this.btnClosed.TabIndex = 12;
+            this.btnClosed.UseVisualStyleBackColor = false;
+            this.btnClosed.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lbCopyright
+            // 
+            this.lbCopyright.Font = new System.Drawing.Font("Calibri", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbCopyright.Location = new System.Drawing.Point(27, 388);
+            this.lbCopyright.Name = "lbCopyright";
+            this.lbCopyright.Size = new System.Drawing.Size(146, 22);
+            this.lbCopyright.TabIndex = 13;
+            this.lbCopyright.Text = "CopyRight 2017 ©";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Calibri", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(656, 388);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 22);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Version 1.2";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(31, 25);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(73, 57);
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // FrmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(779, 430);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbCopyright);
+            this.Controls.Add(this.btnClosed);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.cbNhoPassWord);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(textBox1);
+            this.Controls.Add(this.lbUserName);
+            this.Controls.Add(this.lbTittle);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmDangNhap";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTittle;
+        private System.Windows.Forms.Label lbUserName;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.CheckBox cbNhoPassWord;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.Button btnClosed;
+        private System.Windows.Forms.Label lbCopyright;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
