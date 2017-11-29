308d6cdac09835087a44fc8e4966817942d19796
quocson19977@gmail.com <quocson19977@gmail.com>
2017-11-29 22:07:33 +0700

    update thêm phần xem danh sách nhân viên + cập nhật nhân viên ,theo mô hình 3 lớp

---
+++ QuanLyThuVien/BUS_QuanLy/BUS_Home.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL_QuanLy;
+using DTO_QuanLy;
+
+namespace BUS_QuanLy
+{
+    public class BUS_Home
+    {
+       DAL_Home dalHome = new DAL_Home();
+        public bool updateNV(DTO_Home DTO_Home)
+        {
+            return dalHome.updateNhanVien(DTO_Home);
+
+        }
+    }
+}
